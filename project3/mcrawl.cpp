a549a99a8614f74473b3925f3385dfab25847095
tony-ou <you@example.com>
2018-12-08 23:00:03 -0600

    iasd

--- project3/mcrawl.cpp
+++ project3/mcrawl.cpp
@@ -87,7 +87,7 @@ int main(int argc, char** argv){
         printf("Error creating directory!n");
         exit(1);
     }
-
+    download_queue.push((const char*)"index.html");
     server = gethostbyname(hostname.c_str());        
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;   
@@ -95,83 +95,191 @@ int main(int argc, char** argv){
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(port);
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-         int_err();    
- 
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        int_err();
-    download_queue.push((const char*)"index.html");
-
-
+    crawler();
     return 0;
 }
 
+bool compare(char* c, int len, string cmp){
+    for (int i = 0; i < len; i++)
+    {
+    
+        if (*(c+i) == '\0' || *(c+i) != cmp.at(i))
+            return false;
+    }
+    
+    return true;
+}
+
+char* parse_file(char* to_parse, size_t start, size_t len){
+    size_t cur = 0;
+    bool flag = false;    
+    while (cur < len)
+    {
+
+        char* pos = to_parse + start + cur;
+        char c = *pos;
+        //scout << *pos;  
+       // cout << c << endl;
+        //cout << c;
+            
+        switch (c)
+        {            
+            case 'H':
+                if (compare(pos,4,"HREF"))
+                {
+                    flag = true;
+                    cur += 6;
+                    pos += 6;
+                }
+                break;
+            case 'h':
+                if (compare(pos,4,"href"))
+                {
+                    flag = true;
+                    cur += 6;
+                    pos += 6;
+
+                }
+                break;
+            case 's':
+                if (compare(pos,3,"src"))
+                {
+                    flag = true;
+                    cur += 5;
+                    pos += 5;
+                }
+                break;
+            default:
+                flag = false;
+        }
+        if (flag == false) cur += 1;
+        else 
+        {            
+         
+            char* temp = pos;
+
+            while (*temp != '\0' && *temp != '"'){
+                temp += 1;      
+            }
+            if (*temp == '\0') 
+                {
+
+                    return pos;
+                }
+            else
+            {   
+                *temp = '\0';
+                char* new_file = strdup(pos);                
+                pos = temp + 1;
+                mtx.lock();
+                download_queue.push(new_file);
+                mtx.unlock();            
+                //cout << new_file << endl;
+            }
+        }        
+    }
+    return NULL;
+}
 
 void crawl_html(string to_crawl,int sockfd){
 	// crawl from html, need to parse "to_crawls" from the html and add them to queue
-	char send_data[256], recv_data[1024];
-	snprintf(send_data, sizeof(send_data), "GET /%s HTTP/1.1\r\nHost: eychtipi.cs.uchicago.edu\r\n\r\n",to_crawl); 
-	int n = 0;    
+	char send_data[256], recv_data[30];
+    char* next;
+    char temp_buff[1024];
+    char* temp_ptr = &temp_buff[0];
+    const char* c_hostname = hostname.c_str();
+    const char* c_to_crawl = to_crawl.c_str();
+	snprintf(send_data, sizeof(send_data), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n",c_to_crawl,c_hostname); 
+	int n = 0;        
+  
+    memset(temp_buff,0,sizeof(temp_buff));
 	n = sendto(sockfd, send_data, strlen(send_data), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));            
     while (1)
     {
-        n = recv(sockfd,recv_data,sizeof(recv_data),0);                
-        if (n ==0)
-        {
+        size_t start = 0;
+        n = recv(sockfd,recv_data,sizeof(recv_data),0);                        
+        if (n == 0)        
             break;
-        }
-        cout << recv_data;
-        memset(recv_data,0,sizeof(recv_data));
-	}
-
+        string received = string(recv_data);  
+        if (temp_ptr != &(temp_buff[0]))
+        {        
+            char* next = &recv_data[0];
+            while (*next != '"')
+            {
+                *temp_ptr = *next;
+                next += 1;
+                temp_ptr += 1;
+                start += 1;
+            }            
+            *(temp_ptr++) = '\0';
+                        cout <<   temp_buff << endl;
+            mtx.lock();
+            download_queue.push(temp_buff);
+            mtx.unlock();    
+           // cout << temp_buff << endl;           
+            memset(temp_buff,0,sizeof(temp_buff));             
+            temp_ptr = &temp_buff[0];        
+        }        
+        //cout << recv_data;
+        next = parse_file(recv_data, start, n);
+        if (next != NULL)
+        {
+            //cout << next <<endl;
+          
+            while (*next != '\0')
+            {
+                *temp_ptr = *next;
+                temp_ptr++;
+                next++;
+            }
+        }	   
+    }
 }
 
 void crawl_file(string to_crawl,int sockfd){
 	// download from file 
-
+    return;
 }
 
 void crawler(){
 	const char* temp;
+    string::size_type r_pos, l_pos;
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
          int_err();    
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         int_err();
- 
+
 	while (1){
 		mtx.lock();
 		if (download_queue.empty()) return;
 		temp = download_queue.front();
 		download_queue.pop();
 		mtx.unlock();
-		string to_crawl = str(temp);
+		string to_crawl = string(temp);
+        if (to_crawl.compare("#") == 0)
+            continue;
+        l_pos = to_crawl.find("http://");
+        if (l_pos != string::npos)
+        {
+
+            l_pos += 7;                    
+            r_pos = to_crawl.find("/",l_pos);
+            string h = to_crawl.substr(l_pos,r_pos-l_pos+1);
+            if (h.compare(hostname) != 0)
+                continue;
+        }
 		//convert char* to string
 		if (crawled.count(to_crawl) != 0) continue; //check if crawled
-		crawled.add(to_crawl);
-		string::size_type pos = to_crawl.rfind('.');
-		string file_type = to_crawl.substr(pos,to_crawl.size()-pos);
-		if (string == "htm" || string == "html")
+		crawled.insert(to_crawl);        
+        string::size_type pos = to_crawl.rfind('.');
+		string file_type = to_crawl.substr(pos+1,to_crawl.size()-pos);
+
+
+        if (file_type == "htm" || file_type == "html"){
 			crawl_html(to_crawl,sockfd);
+        }
 		else crawl_file(to_crawl,sockfd); 
-
-
-		snprintf(send_data, sizeof(send_data), "GET /%s HTTP/1.1\r\nHost: eychtipi.cs.uchicago.edu\r\n\r\n",to_crawl); 
-		
-
-		int n = 0;    
-	    n = sendto(sockfd, send_data, strlen(send_data), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));            
-	    while (1)
-	    {
-	        n = recv(sockfd,recv_data,sizeof(recv_data),0);                
-	        if (n ==0)
-	        {
-	            break;
-	        }
-	        cout << recv_data;
-	        memset(recv_data,0,sizeof(recv_data));
-	    }
 	}
 }
 /* instruction for creating file 
